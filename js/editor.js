22732fad2adff5cd0ace06ed2205079590d4e2f7
Ripon Mondal <riponmondal149@gmail.com>
2023-08-23 04:14:04 +0600

    make text italic to click button

--- js/editor.js
+++ js/editor.js
@@ -1,5 +1,12 @@
-// make bold to click button
+// make text bold to click button
 document.getElementById('btn-bold').addEventListener('click', function () {
     const outputBox = document.getElementById('output-box');
     outputBox.style.fontWeight = '700';
 })
+
+// make text italic to click button
+document.getElementById('btn-italic').addEventListener('click', function () {
+    const outputBox = document.getElementById('output-box');
+    outputBox.style.fontStyle = 'italic';
+})
+
