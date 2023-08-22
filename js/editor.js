6e58cdfc609aed428f2805290d04246331cb968e
Ripon Mondal <riponmondal149@gmail.com>
2023-08-23 04:09:59 +0600

    make text bold clicking button

---
+++ js/editor.js
@@ -0,0 +1,5 @@
+// make bold to click button
+document.getElementById('btn-bold').addEventListener('click', function () {
+    const outputBox = document.getElementById('output-box');
+    outputBox.style.fontWeight = '700';
+})
